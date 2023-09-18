23abd4cb9f713718ef6da8ec83d53bee4c9e74e7
xuwb <xuwb@superb-pay.com>
2023-09-18 16:07:30 +0800

    1

--- code/updateOtherGit.bat
+++ code/updateOtherGit.bat
@@ -2,7 +2,6 @@
 setlocal
 
 call :DownloadAndRename "https://raw.githubusercontent.com/q7q7q7q7q7q7q7/ziyou/main/œ≤∞Æ∞Ô.py" "xiaiban.py" "xiaiban.py"
-call :DownloadAndRename "https://raw.githubusercontent.com/meetclover/JavaScript/main/mcyp.py" "mcyp.py" "mcyp.py"
 call :DownloadAndRename "https://raw.githubusercontent.com/q7q7q7q7q7q7q7/ziyou/main/µŒµŒ.py" "didi.py" "didi.py"
 call :DownloadAndRename "https://raw.githubusercontent.com/smallfawn/QLScriptPublic/main/ø®»¯µ€«©µΩÃ·œ÷.py" "kasadi.py" "kasadi.py"
 call :DownloadAndRename "https://raw.githubusercontent.com/241793/bucai2/main/÷–Ω°365.py" "zj.py" "zj.py"
