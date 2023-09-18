db0fa9a10800fd6b540203be62657ff802c80dde
xuwb <xuwb@superb-pay.com>
2023-09-18 17:12:28 +0800

    1

--- code/History.py
+++ code/History.py
@@ -1,5 +1,5 @@
 import requests
 
-url = 'https://springboot-weather-push-gftlgclvaw.cn-hangzhou.fcapp.run/History'
-test = requests.get(url=url)
+url = "https://springboot-weather-push-gftlgclvaw.cn-hangzhou.fcapp.run/History"
+test = requests.get(url=url123123)
 print(test.text)
